d3eacff0a4d6e3af76ecdf21e6a3b37e5fb0f0ef
Teal <git@teal.is>
2022-12-10 20:03:47 +0100

    Fix transfers command

--- putio_automator/commands/transfers.py
+++ putio_automator/commands/transfers.py
@@ -1,16 +1,17 @@
 """
 Commands for managing transfers on Put.IO.
 """
+
 import click
 import yaml
 
-from putio_automator import date_handler
 from putio_automator.cli import cli
 
 
 @cli.group()
 def transfers():
     pass
+
 
 @transfers.command()
 @click.pass_context
@@ -29,15 +30,20 @@ def cancel_by_status(ctx, statuses):
     if len(transfer_ids):
         ctx.obj['CLIENT'].Transfer.cancel_multi(transfer_ids)
 
-@transfers.command()
-def cancel_completed():
-    "Cancel completed transfers"
-    cancel_by_status('COMPLETED')
 
 @transfers.command()
-def cancel_seeding():
+@click.pass_context
+def cancel_completed(ctx):
+    "Cancel completed transfers"
+    ctx.invoke(cancel_by_status, 'COMPLETED')
+
+
+@transfers.command()
+@click.pass_context
+def cancel_seeding(ctx):
     "Cancel seeding transfers"
-    cancel_by_status('SEEDING')
+    ctx.invoke(cancel_by_status, 'SEEDING')
+
 
 @transfers.command()
 @click.pass_context
@@ -45,12 +51,14 @@ def clean(ctx):
     "Clean finished transfers"
     ctx.obj['CLIENT'].Transfer.clean()
 
+
 @transfers.command()
 @click.pass_context
 def groom(ctx):
     "Cancel seeding and completed transfers, and clean afterwards"
-    ctx.invoke(cancel_by_status, ['SEEDING', 'COMPLETED'])
+    ctx.invoke(cancel_by_status, statuses=['SEEDING', 'COMPLETED'])
     ctx.invoke(clean)
+
 
 @transfers.command()
 @click.pass_context
